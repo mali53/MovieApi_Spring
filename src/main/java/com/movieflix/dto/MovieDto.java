75e9639b01ba88131c288094db9bb09c529e76a0
Ravindu Malinga <ravindu_d@epiclanka.net>
2024-07-01 08:37:49 +0530

    Initial Commit

---
+++ src/main/java/com/movieflix/dto/MovieDto.java
@@ -0,0 +1,45 @@
+package com.movieflix.dto;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDto {
+
+    private Integer movieId;
+
+
+    @NotBlank(message = "Please provide movie's title!")
+    private String title;
+
+
+    @NotBlank(message = "Please provide movie's director!")
+    private String director;
+
+
+    @NotBlank(message = "Please provide movie's studio!")
+    private String studio;
+
+
+    private Set<String> movieCast;
+
+
+    @NotBlank(message = "Please provide movie's release year!")
+    private Integer releaseYear;
+
+
+    @NotBlank(message = "Please provide movie's poster!")
+    private String poster;
+
+    @NotBlank(message = "Please provide poster's url!")
+    private String posterUrl;
+}
